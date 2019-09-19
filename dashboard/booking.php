7385a2355cf8c739fcdb6e0bc1fdb2950d3cf5ed
Daiva Haryanto <haryantodaiva0@gmail.com>
2019-09-19 20:58:36 +0700

    booking page

--- dashboard/booking.php
+++ dashboard/booking.php
@@ -1,7 +1,183 @@
 <?php include '../layout/dashboard.php';?>
-<div class="homeStyle">
-        <h1>Bookong</h1>
-    </div>
+        <div class="container" id="gallery">
+            <h1 class="title is-1 has-text-centered">Choose Your Style</h1>
+            <div class="columns">
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Slicked Back Undercut</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Slicked_Back_Undercut.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Long Comb + Low Fade</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Long_Comb_Over_Low_Fade.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Long Special Hairstyle</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Long_Hairstyle.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Medium Side Swept</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Medium_Side_Swept.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Messy Taper Hairstyle</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Messy_Taper_Hairstyle.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Short Haircut + Undercut</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Short_Haircut_Combed_Over_Undercut.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Short Special Quiff</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Short_Quiff.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Short Sides Long Top</p>
+                        <figure class="image">  
+                            <img src="../css/Hair-Style/Short_Sides_Long_Top.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Short Special Textured</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Short_Textured.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Slicked Back Wavy</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Slicked_Back_Wavy.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Textured Pompadour</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Textured_Pompadour_Fade.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Textured Crop Top Fade</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Textured_Crop_Top_Fade.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Faux Hawk Fade</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Faux_Hawk_Fade.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Spiky Hair Special Fade</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Spiky_Hair_Fade.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Side Part + High Bald Fade</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Side_Part_Hairstyle_High_Bald_Fade.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+                <div class="column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification">
+                        <p class="title is-6">Slick Back With Flow</p>
+                        <figure class="image">
+                            <img src="../css/Hair-Style/Slick_Back_With_Flow.png">
+                        </figure>
+                        </article>
+                    </div> 
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+ 
+
+
     </body>
     </html>
     <style lang="css">
