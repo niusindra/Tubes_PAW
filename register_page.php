63e547cc2f213792a86fa5eb895d3f8eb7faf3e0
niusindra <38039465+niusindra@users.noreply.github.com>
2019-09-20 00:13:37 +0700

    navbar ireng

--- register_page.php
+++ register_page.php
@@ -11,12 +11,20 @@
     </head>
   <body>
   <div class="backgroundAll"></div>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
           <a class="navbar-item" href="./">
               <img src="https://bulma.io/images/bulma-logo.png" width="112"
               height="28">
             </a>
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+            
+            <div class="navbar-start">
+                <a class="navbar-item" href="./index.php">
+                    Home
+                </a>
+            </div>
         </div>
     </nav>
     <div class="columns" style="margin-top: 80px;">
