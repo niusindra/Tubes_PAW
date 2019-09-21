acd3d7001c6e7e9d2ed35d8169f7b36861568c83
niusindra <38039465+niusindra@users.noreply.github.com>
2019-09-21 23:34:10 +0700

    halaman profile

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -1,7 +1,37 @@
-<?php include '../layout/dashboarduser.php';?>
-    <div class="homeStyle">
-        
-    </div>
+<?php include '../layout/dashboarduser.php';
+$id=$_GET['id']; 
+        $query = mysqli_query($con, "SELECT * FROM users where id='$id'") or die(mysqli_error($con)); 
+        $data = mysqli_fetch_assoc($query)?>
+
+        <div class="columns" style="margin-top: 80px;">
+            <div class="column is-one-quarter" style="margin:auto;">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Your Profile
+                    </p>
+                </header>
+                <form action="./proses/prosesRegister.php" method="POST">
+                <div class="card-content">
+                    <div class="field">
+                        <p class="control">
+                            <label class="label">Name</label>
+                            <input class="input" name="name"  type="text" placeholder="Name" value=<?php echo $data['username'] ?>>
+                        </p>
+                        </div>
+                        <div class="field">
+                         <p class="control">
+                             <label class="label">Email</label>
+                             <input name="email" class="input" type="email" placeholder="Email" value=<?php echo $data['email'] ?>>
+                        </p>
+                        </div>     
+                        </div>
+                        <footer class="card-footer">
+                            <input name="store" type="submit" value="Edit Profile" style="padding: 0px !important" class="card-footer-item button is-dark">
+                        </footer>
+                </div>
+                </form>
+
     </body>
     </html>
     <style lang="css">
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/bulma-0.7.5/css/bulma.min.css">
     
-    <title>Home</title>
+    <title>BarBarbershop</title>
 </head>
   <body>
   <div class="backgroundAll"></div>
